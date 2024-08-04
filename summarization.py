249c8321ebe5af543bf1fd4c5eff20f6c3d16365
cam <cam@cams-MacBook-Pro.local>
2024-08-03 18:33:07 -0700

    build out summarization function

--- summarization.py
+++ summarization.py
@@ -4,6 +4,8 @@ from openai import OpenAI
 
 
 
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"),)
+
 PROMPT = """
 The following is the transcription of a police radio conversation. The conversation was recorded on a noisy radio, which means the transcription will have inaccuracies. For instance, the word "copy" is often incorrectly transcribed as "coffee." Additionally, words or phrases that occur at the beginning or end of the transcription might be clipped, as this is where the recording began or ended. Also, there are ads inserted into the conversations. If you see an add, don't describe it, simply write "NOW FOR A COMMERCIAL BREAK!"
 
@@ -12,29 +14,26 @@ I want you to summarize this conversation. Use declarative sentences. Do not exp
 
 """
 
+
 def get_summary(text):
     """
     Summarizes some text.
     """
     return text
+    completion = client.completions.create(
+        model = "gpt-3.5-turbo-instruct",
+        prompt = f"{PROMPT}: {text}",
+        max_tokens = 10,
+        temperature = 0
+    )
+
+    return completion.choices[0].text.strip()
 
 
 
-# os.environ["OPENAI_API_KEY"] = key
+if __name__ == "__main__":
+    dummy_convo = ""
 
+    response = get_summary(dummy_convo)
 
-
-
-# client = OpenAI(
-#     api_key = os.getenv("OPENAI_API_KEY"),
-# )
-
-# completion = client.completions.create(
-#     model = "gpt-3.5-turbo-instruct",
-#     prompt = f"please sumamrize the following text: {text}",
-#     max_tokens = 7,
-#     temperature = 0
-# )
-
-# print(completion.choices[0].text.strip())
-
+    print(response)
